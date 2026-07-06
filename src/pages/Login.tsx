54cd5b3f1457dc2798527c50a6d9173211c6c0f4
loanth <145116258+loanth@users.noreply.github.com>
2026-07-06 08:52:09 +0200

    testPipeline2

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -35,6 +35,7 @@ export const Login: React.FC = () => {
       );
 console.log('Selected Country Config:', countryConfig);
 console.log('Test pipeline');
+console.log('Test pipeline');
       if (!countryConfig) {
         setError('Pays introuvable');
         return;
